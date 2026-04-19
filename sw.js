2052a01079a1a76e70ab59fa6c79ed6942c4675a
sadmansamy16 <sadmansamy16@gmail.com>
2026-04-19 13:44:08 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'samy-root-v1';
+const CACHE_NAME = 'samy-v2';
 
 self.addEventListener('install', (event) => {
   self.skipWaiting();
@@ -7,12 +7,11 @@ self.addEventListener('install', (event) => {
 self.addEventListener('fetch', (event) => {
   const url = new URL(event.request.url);
 
-  // If the user is trying to access /chat, the root PWA must ignore it
+  // Still ignore chat
   if (url.pathname.startsWith('/chat')) {
-    return; 
+    return;
   }
 
-  // Handle other requests (root, /2fa, etc)
   event.respondWith(
     fetch(event.request).catch(() => {
       return caches.match(event.request);
