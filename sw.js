16e521623c9eaf64e885e99522c88e508a806c1f
sadmansamy16 <sadmansamy16@gmail.com>
2026-05-01 12:49:00 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,26 @@
-const CACHE_NAME = 'samy-v1';
-const assets = ['/', '/index.html', '/manifest.json', '/redirect.json'];
+const CACHE_NAME = 'samy-dynamic-cache';
 
-self.addEventListener('install', event => {
-    event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
+self.addEventListener('install', (e) => {
+    self.skipWaiting();
 });
 
-self.addEventListener('fetch', event => {
-    event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
+self.addEventListener('activate', (e) => {
+    e.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        fetch(event.request)
+            .then((response) => {
+                // If network works, clone it to cache and return
+                return caches.open(CACHE_NAME).then((cache) => {
+                    cache.put(event.request, response.clone());
+                    return response;
+                });
+            })
+            .catch(() => {
+                // If network fails (offline), use cache
+                return caches.match(event.request);
+            })
+    );
 });
