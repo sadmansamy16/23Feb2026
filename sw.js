6eeda62e5c437c7fea424b23f5266a012265b78d
sadmansamy16 <sadmansamy16@gmail.com>
2026-05-01 12:04:33 +0600

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'samy-cache-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/links.json',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
