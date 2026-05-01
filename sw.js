89f06e1992df3ad203174580469cc8bd05d7f29c
sadmansamy16 <sadmansamy16@gmail.com>
2026-05-01 12:19:32 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,10 @@
-const CACHE_NAME = 'samy-cache-v1';
-const ASSETS = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/links.json',
-  '/icon-192.png',
-  '/icon-512.png'
-];
+const CACHE_NAME = 'samy-v1';
+const assets = ['/', '/index.html', '/manifest.json', '/redirect.json'];
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+self.addEventListener('install', event => {
+    event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
-  );
+self.addEventListener('fetch', event => {
+    event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
 });
