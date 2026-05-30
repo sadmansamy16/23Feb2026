e1268bc75ecb0d06ce253f328f136fc0b6b2f47a
sadmansamy16 <sadmansamy16@gmail.com>
2026-05-30 18:59:40 +0600

    Create search_sw.js

---
+++ p/search_sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'samy-search-v1';
+const ASSETS = [
+  'search.html',
+  'search_manifest.json',
+  'p/search_web_list.json',
+  'p/search_web_img.json'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
