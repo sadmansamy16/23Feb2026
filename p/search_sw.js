a7646d39b356833599b6f381e0afd0af9f556128
sadmansamy16 <sadmansamy16@gmail.com>
2026-05-30 19:15:18 +0600

    Update search_sw.js

--- p/search_sw.js
+++ p/search_sw.js
@@ -1,9 +1,9 @@
 const CACHE_NAME = 'samy-search-v1';
 const ASSETS = [
-  'search.html',
-  'search_manifest.json',
-  'p/search_web_list.json',
-  'p/search_web_img.json'
+  '/p/search.html',
+  '/p/search_manifest.json',
+  '/p/search_web_list.json',
+  '/p/search_web_img.json'
 ];
 
 self.addEventListener('install', (e) => {
